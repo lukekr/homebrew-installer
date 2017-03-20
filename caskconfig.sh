88d9007110eafc2edcc794e2a59bb90c0bab3807
lukekr <lkramer@c02p42pyg3qp.resource.com>
2017-03-20 16:11:07 -0400

    Initial Commit

---
+++ caskconfig.sh
@@ -0,0 +1,30 @@
+
+#!/bin/sh
+
+<br>brew cask install slack
+<br>brew cask install alfred
+<br>brew cask install evernote
+<br>brew cask install atom
+<br>brew cask install google-chrome
+<br>brew cask install blender
+<br>brew cask install adium
+<br>brew cask install skype
+<br>brew cask install sqlitebrowser
+<br>brew cask install vlc
+<br>brew cask install spotify
+<br>brew cask install dropbox
+<br>brew cask install utorrent
+<br>brew cask install the-unarchiver
+<br>brew cask install filezilla
+<br>brew cask install google-drive
+<br>brew cask install invisionsync
+<br>brew cask install iconjar
+<br>brew cask install licecap
+<br>brew cask install mamp
+<br>brew cask install mockuuups-studio
+<br>brew cask install neofinder
+<br>brew cask install principle
+<br>brew cask install sip
+<br>brew cask install sketch-toolbox
+<br>brew cask install skyfonts
+<br>brew cask install adobe-creative-cloud
